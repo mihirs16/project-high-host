7b1fd5419e9d3d15952737d157b1951756137ae8
Mihir Singh <mihirs16@gmail.com>
2020-04-03 10:44:36 +0530

    Update Models and API

--- model_pred.py
+++ model_pred.py
@@ -2,7 +2,7 @@ import model_init as mod_i
 import model_pickle_files as mod_pic
 import corpora_machine as corps
 
-from nltk.stem import PorterStemmer
+#from nltk.stem import WordNetLemmatizer
 
 # model first train
 # import model_train
@@ -13,12 +13,18 @@ text_idf = corps.corpora_train()
 # model predict and generate tag list
 ml_features, ml_features_models = mod_i.load_tag_machines()
 
+# tag object
+class tag:
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+
 def text_return_tags(text, title):
 
     # clean text
     cleaned_text = corps.clean_text(text)
     cleaned_title = corps.clean_text(title)
-    keywd_by_freq = corps.freq_dist(cleaned_text)
+    keywd_by_freq, scor_by_freq = corps.freq_dist(cleaned_text)
 
     # new text to features
     text_ft = text_idf.transform([cleaned_text])
@@ -31,57 +37,63 @@ def text_return_tags(text, title):
         # text features from articles
         y_pred = ml_features_models[model_index].predict(text_ft)
         if y_pred == 1:
-            tag_list.append(ml_features[model_index].lower())
+            tag_list.append(tag(ml_features[model_index].lower(), 0.8))
 
 
-    global_tags = list(corps.pd.read_csv('global_taglist.csv')['global_taglist'])
+    global_tags = list(corps.pd.read_csv('project_high/Model/global_taglist.csv')['global_taglist'])
     for i in range(0, len(global_tags)):
         y = str(global_tags[i]).lower()
 
         # title
         if y in title.lower() or y in cleaned_title:
-            tag_list.append(global_tags[i].lower())
-        
+            tag_list.append(tag(global_tags[i].lower(), 0.85))
+            
         # frequency distribution
         if y in keywd_by_freq:
-            tag_list.append(global_tags[i].lower())
+            tag_list.append(tag(global_tags[i].lower(), scor_by_freq[keywd_by_freq.index(y)]))
 
     # get rid of duplicates
-    tag_list = list(mod_i.np.unique(tag_list))
+    tag_list = list(set(tag_list))
+    tag_data_dict = []
+    for _tag in tag_list:
+        tag_data = {"name": _tag.name, "score": _tag.score}
+        tag_data_dict.append(tag_data)
 
     # return tags
-    return tag_list
+    return tag_data_dict
 
-# def test_webscraper_function(url):
-#     import selenium
-#     import bs4
-#     from bs4 import BeautifulSoup
-#     from selenium import webdriver
+def test_webscraper_function(url):
+    import selenium
+    import bs4
+    from bs4 import BeautifulSoup
+    from selenium import webdriver
 
-#     # Getting Pages
-#     driver = webdriver.Chrome('chromedriver.exe')
-#     driver.get(url)
-#     res = driver.execute_script("return document.documentElement.outerHTML")
-#     driver.quit()
+    # Getting Pages
+    driver = webdriver.Chrome('project_high/Model/chromedriver.exe')
+    driver.get(url)
+    res = driver.execute_script("return document.documentElement.outerHTML")
+    driver.quit()
 
-#     # Parse Page
-#     soup = BeautifulSoup(res, 'lxml')
+    # Parse Page
+    soup = BeautifulSoup(res, 'lxml')
 
-#     # Text
-#     para = soup.findAll('p')
-#     text = ''
-#     for p in para:
-#         text = text + ' ' + p.getText()
-#     # text = text_processor(text)
+    # Text
+    para = soup.findAll('p')
+    text = ''
+    for p in para:
+        text = text + ' ' + p.getText()
+    # text = text_processor(text)
 
-#     try:
-#             name = soup.find('h1').getText()
-#     except:
+    try:
+            name = soup.find('h1').getText()
+    except:
         
-#         name = 'None'
+        name = 'None'
 
-#     return text, name
+    return text, name
 
-# # local testing
-# text, title = test_webscraper_function('https://medium.com/@austincoleschafer/the-psychology-of-selling-1bfea4cd127')
-# print(text_return_tags(text, title))
+# local testing
+text, title = test_webscraper_function('https://medium.com/@lukegardner/kill-content-marketing-replace-it-with-knowledge-marketing-e56b55e06b10?source=extreme_main_feed---------3-59--------------------ace2d49d_42a1_447b_a90a_323911908333--7')
+
+hm = text_return_tags(text, title)
+print(hm)
